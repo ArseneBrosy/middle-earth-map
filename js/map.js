841d6b3efe42238f4107a40c579048eb44d02828
Arsène Brosy <118964023+ArseneBrosy@users.noreply.github.com>
2026-03-31 15:22:37 +0200

    add map 0

--- js/map.js
+++ js/map.js
@@ -1,8 +1,13 @@
 let map = L.map('map', {
-    crs: L.CRS.simple
+    crs: L.CRS.Simple,
+    minZoom: 0,
+    maxZoom: 2
 })
 
-L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-    maxZoom: 19,
-    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+let bounds = [[0,0], [1024, 1024]];
+
+L.imageOverlay('../img/map/0/0-0.png', {
+    bounds: bounds
 }).addTo(map);
+
+map.fitBounds(bounds);
